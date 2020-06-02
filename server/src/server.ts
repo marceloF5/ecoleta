efd46f1385211e04488f47f884d776ca25b1c006
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-06-02 23:59:32 +0100

    feat(server): implements server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express'
+import path from 'path'
+import routes from './routes'
 
 const app = express()
 
-app.get('/users', (request, response) => {
-  console.log('User List')
+app.use(express.json())
+app.use(routes)
 
-  response.json(['Marcelo', 'Yasmin', 'Hugo'])
-})
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333, () => {
   console.log('Listen on port 3333')
