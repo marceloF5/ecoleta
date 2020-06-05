da9a047a8019b9f7a7796fc3bb3d1e364797bdd6
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-06-05 22:34:47 +0100

    chore(web): implements web

---
+++ web/src/pages/Home/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { FiLogIn } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import './styles.css'
+import logo from '../../assets/logo.svg'
+
+const Home = () => {
+  return (
+    <div id='page-home'>
+      <div className='content'>
+        <header>
+          <img src={logo} alt='Ecoleta' />
+        </header>
+        <main>
+          <h1>Your garbage collection marketplace </h1>
+          <p>We help people find collection points efficiently</p>
+          <Link to='create-point'>
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Register a collect point</strong>
+          </Link>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default Home
